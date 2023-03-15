76355327dc122f8b9538d88332dd13fcbe0db0af
Emil Hofstetter <emil.k.hofstetter@gmail.com>
2023-03-14 20:56:10 -0400

    add std hashmap to tests

--- benches/caching.rs
+++ benches/caching.rs
@@ -1,5 +1,7 @@
 use std::collections::hash_map::RandomState;
+use std::collections::HashMap;
 use std::hash::Hash;
+use std::sync::Mutex;
 use std::thread;
 
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
@@ -8,7 +10,7 @@ use rand::{thread_rng, Rng};
 use moka::sync::Cache as MokaCache;
 use seckoo::HashMap as SeckooCache;
 
-const THREADS: [usize; 8] = [1, 2, 3, 4, 5, 6, 7, 8];
+const THREADS: [usize; 5] = [1, 2, 4, 8, 16];
 
 pub trait Cache: Send + Sync {
     type Key;
@@ -65,6 +67,23 @@ where
 
     fn get(&self, k: &Self::Key) -> Option<Self::Value> {
         MokaCache::get(&self, k)
+    }
+}
+
+impl<K, V> Cache for Mutex<HashMap<K, V>>
+where
+    K: Hash + Eq + Send + Sync + 'static,
+    V: Clone + Send + Sync + 'static,
+{
+    type Key = K;
+    type Value = V;
+
+    fn insert(&self, k: Self::Key, v: Self::Value) {
+        HashMap::insert(&mut self.lock().unwrap(), k, v);
+    }
+
+    fn get(&self, k: &Self::Key) -> Option<Self::Value> {
+        HashMap::get(&self.lock().unwrap(), k).cloned()
     }
 }
 
@@ -145,12 +164,17 @@ pub fn compare_insert_get(c: &mut Criterion) {
         group.bench_with_input(
             BenchmarkId::from_parameter(format!("Seckoo {t} Thread(s)")),
             &t,
-            |b, &t| b.iter(|| insert_get_random(t, || SeckooCache::<_, _, _, 4096>::std())),
+            |b, &t| b.iter(|| insert_get_random(t, || SeckooCache::<_, _, _, 8192>::std())),
+        );
+        group.bench_with_input(
+            BenchmarkId::from_parameter(format!("Std-Mutex {t} Thread(s)")),
+            &t,
+            |b, &t| b.iter(|| insert_get_random(t, || Mutex::new(HashMap::new()))),
         );
         group.bench_with_input(
             BenchmarkId::from_parameter(format!("Moka {t} Thread(s)")),
             &t,
-            |b, &t| b.iter(|| insert_get_random(t, || MokaCache::new(4096))),
+            |b, &t| b.iter(|| insert_get_random(t, || MokaCache::new(8192))),
         );
     }
     group.finish();
@@ -165,6 +189,11 @@ pub fn compare_insert(c: &mut Criterion) {
             |b, &t| b.iter(|| insert_random(t, || SeckooCache::<_, _, _, 8192>::std())),
         );
         group.bench_with_input(
+            BenchmarkId::from_parameter(format!("Std-Mutex {t} Thread(s)")),
+            &t,
+            |b, &t| b.iter(|| insert_random(t, || Mutex::new(HashMap::new()))),
+        );
+        group.bench_with_input(
             BenchmarkId::from_parameter(format!("Moka {t} Thread(s)")),
             &t,
             |b, &t| b.iter(|| insert_random(t, || MokaCache::new(8192))),
@@ -176,6 +205,7 @@ pub fn compare_insert(c: &mut Criterion) {
 pub fn compare_get(c: &mut Criterion) {
     let seckoo_cache = SeckooCache::<_, _, _, 8192>::std();
     let moka_cache = MokaCache::new(8192);
+    let std_mutex = Mutex::new(HashMap::new());
 
     let mut rng = thread_rng();
 
@@ -185,12 +215,17 @@ pub fn compare_get(c: &mut Criterion) {
         moka_cache.insert(black_box(num), i);
     }
 
-    let mut group = c.benchmark_group(format!("Compare Insert"));
+    let mut group = c.benchmark_group(format!("Compare Get"));
     for t in THREADS {
         group.bench_with_input(
             BenchmarkId::from_parameter(format!("Seckoo {t} Thread(s)")),
             &t,
             |b, &t| b.iter(|| get_random(t, || &seckoo_cache)),
+        );
+        group.bench_with_input(
+            BenchmarkId::from_parameter(format!("Std-Mutex {t} Thread(s)")),
+            &t,
+            |b, &t| b.iter(|| get_random(t, || &std_mutex)),
         );
         group.bench_with_input(
             BenchmarkId::from_parameter(format!("Moka {t} Thread(s)")),
